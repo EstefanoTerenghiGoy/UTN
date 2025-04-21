c8b2f82f0deb085d2bb7c6523b5755f20f281bd6
DarksRoth <70455929+DarksRoth@users.noreply.github.com>
2025-04-21 20:12:40 -0300

    Recursivo

---
+++ Div 213/Funciones Recursivas/ej1.py
@@ -0,0 +1,16 @@
+# def sumar_naturales(numero: int)->int:
+#     if numero >= 0:
+#         num_final+=numero
+#         sumar_naturales(numero-1)
+#     return num_final
+
+# numero = int(input("Ingrese el número para sumar desde 1 hasta él: "))
+# print(sumar_naturales(numero))
+
+def sumar_naturales(numero: int) -> int:
+    if numero <= 0:
+        return 0
+    return numero + sumar_naturales(numero - 1)
+
+numero = int(input("Ingrese el número para sumar desde 1 hasta él: "))
+print(sumar_naturales(numero))
